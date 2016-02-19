f5ab91c235f8ce3de001de7b8163b5f39f6f534c
petr0vsk <petr0vskjy.aleksander@gmail.com>
2016-02-19 21:24:42 +0300

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -42,27 +42,3 @@ cacheSolve <- function(x, ...) {
     # Return this new result
     m
 }
-## -------------------- operability test --------------------------------------------------------------
-## > source("LexScop.R")    load R program
-## > a <- makeCacheMatrix() create functions
-## > a$set(diag(7,3))       create matrix in working environment
-## OR create matrix another way: a <- makeCacheMatrix(matrix(c(7,0,0,0,7,0,0,0,7), nrow = 3, ncol =3))
-## > a$get()                look at matrix
-##      [,1] [,2] [,3]
-## [1,]    7    0    0
-## [2,]    0    7    0
-## [3,]    0    0    7
-## > cacheSolve(a)          1st run returns inverted matrix
-##                          from working environment
-##           [,1]      [,2]      [,3]
-## [1,] 0.1428571 0.0000000 0.0000000
-## [2,] 0.0000000 0.1428571 0.0000000
-## [3,] 0.0000000 0.0000000 0.1428571
-##
-## > cacheSolve(a)          2nd and subsequent runs
-##                          returns inverted matrix from cache
-## getting cached data
-##           [,1]      [,2]      [,3]
-## [1,] 0.1428571 0.0000000 0.0000000
-## [2,] 0.0000000 0.1428571 0.0000000
-## [3,] 0.0000000 0.0000000 0.1428571
